d24044b9692ca2160fc536e6e06d5f12e3e03a89
AmgaadShokrallah <amgaad@example.com>
2023-11-06 06:01:17 -0800

    commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end="")
-                if j != (len(matrix[i]) - 1):
+    for d in range(len(matrix)):
+        for j in range(len(matrix[d])):
+                print("{:d}".format(matrix[d][j]), end="")
+                if j != (len(matrix[d]) - 1):
                     print(" ", end="")
 
 
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,10 @@
 def max_integer(my_list=[]):
     if len(my_list) < 1:
         return (None)
-        
-    list_copy = my_list.copy()
-    list_copy.sort()
-    return (list_copy(-1)
+
+    list_copy = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > list_copy:
+            list_copy = my_list[i]
+
+    return (list_copy)
