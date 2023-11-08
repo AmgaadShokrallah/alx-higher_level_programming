8d3a72cc88de92bfa411a71bb108bb5e7596d679
AmgaadShokrallah <amgaad@example.com>
2023-11-08 06:15:20 -0800

    commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
 
-    maxv = list(a_dictionary.keys())[0]
-    maxk = a_dictionary[maxv]
+    maxk = list(a_dictionary.keys())[0]
+    maxv = a_dictionary[maxk]
     for k, v in a_dictionary.items():
-        if v > maxk:
-            maxk = v
-            maxv = k
-    return (maxv)
+        if v > maxv:
+            maxv = v
+            maxk = k
+    return (maxk)
